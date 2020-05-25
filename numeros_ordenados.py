97205321c699560a3ef081d60b84cd541d74cb74
Wilker Xavier <willcostaxavier@hotmail.com>
2020-05-24 23:30:30 -0300

    exercicio aula 3 - numeros em ordem crescente

---
+++ numeros_ordenados.py
@@ -0,0 +1,8 @@
+a = int(input("Digite um inteiro 1: "))
+b = int(input("Digite um inteiro 2: "))
+c = int(input("Digite um inteiro 3: "))
+
+if ((a < b) < c ) and (b < c):
+        print(" Ordem Crescente !!!")
+else:
+        print("Não esta na ordem crescente")

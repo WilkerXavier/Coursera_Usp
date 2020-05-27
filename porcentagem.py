82a38724a7a43a206a4c82e9c0560ac87d387389
Wilker Xavier <willcostaxavier@hotmail.com>
2020-05-27 18:32:16 -0300

    função matemática de porcentagem

---
+++ porcentagem.py
@@ -0,0 +1,8 @@
+valor = int(input("Entre com um número para saber o valor: ")) 
+porcentagem = int(input('Entre com um número para saber a porcentagem: '))
+
+multiplica = porcentagem * valor
+
+divide = multiplica / 100 
+
+print(divide)

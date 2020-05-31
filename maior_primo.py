2d64281aa77ae36bb5cc8dc0989fd8116760a340
Wilker Xavier <willcostaxavier@hotmail.com>
2020-05-30 21:23:10 -0300

    Exercicios de Funções

---
+++ maior_primo.py
@@ -0,0 +1,5 @@
+def maior_primo(n):
+    if n % 1 != 0 or n % n != 0:
+        return n
+    
+    
---
+++ vogais.py
@@ -0,0 +1,8 @@
+def vogal(c):
+    vogais = "a","e","i","o","u" 
+       
+    if c == vogais :
+        return False
+    else:
+        return True
+    

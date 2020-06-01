0e53d51cea051f2695fdabfa70365dfb0e18512d
Wilker Xavier <willcostaxavier@hotmail.com>
2020-05-31 21:44:13 -0300

    Refatração

--- bhaskara.py
+++ bhaskara.py
@@ -1,20 +1,25 @@
 import math
 
-a = float(input("Digite o valor de a: "))
-b = float(input("Digite o valor de b: "))
-c = float(input("Digite o valor de c: "))
+def delta(a,b,c):
+    return b ** 2 - 4 * a * c
 
-delta = b ** 2 - 4 * a * c
+def main():
+    a = float(input("Digite o valor de a: "))
+    b = float(input("Digite o valor de b: "))
+    c = float(input("Digite o valor de c: "))
+    imprime_raizes(a,b,c)
 
-if delta == 0:
-    raiz1 = (-b + math.sqrt(delta)) / (2* a)
-    print("A unica raiz é: ", raiz1)
-else:
-    if delta < 0:
-        print("Está equação não  possui raizes reais")
+def imprime_raizes(a,b,c):
+    d = delta(a,b,c)
+    if d == 0:
+        raiz1 = (-b + math.sqrt(d) / (2* a)
+        print("A unica raiz é: ", raiz1)
     else:
-        raiz1 = (-b + math.sqrt(delta)) / (2* a)
-        raiz2 = (-b - math.sqrt(delta)) / (2* a)
+        if d < 0:
+            print("Está equação não  possui raizes reais")
+        else:
+            raiz1 = (-b + math.sqrt(d)) / (2* a)
+            raiz2 = (-b - math.sqrt(d)) / (2* a)
 
-        print ("A primeira raiz é: ", raiz1)
-        print ("A segunda raiz é: ", raiz2)
+            print ("A primeira raiz é: ", raiz1)
+            print ("A segunda raiz é: ", raiz2)
